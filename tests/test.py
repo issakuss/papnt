54270d731efe8067f1bb982bdd073325ffafc1a5
issakuss <issakuss@gmail.com>
2025-12-30 14:59:40 +0900

    refactor: Test using CliRunner

--- tests/test.py
+++ tests/test.py
@@ -1,52 +1,130 @@
-from pathlib import Path
 import unittest
+from unittest.mock import patch, MagicMock
+import shutil
+from tempfile import mkdtemp
+from pathlib import Path
+import tomllib
 
-from papnt.misc import load_config
-from papnt.database import NotionDatabase, DatabaseInfo
-from papnt.mainfunc import (
-    add_records_from_local_pdfpath,
-    update_unchecked_records_from_doi,
-    update_unchecked_records_from_uploadedpdf,
-    make_bibfile_from_records,
-    make_abbrjson_from_bibpath,
-)
+from click.testing import CliRunner
 
-config = load_config('papnt/config.ini')
-database = NotionDatabase(DatabaseInfo())
+from papnt.cli import main
+from papnt.database import NotionDatabase
+from papnt.notionprop import to_notionprop
 
 
-class Test(unittest.TestCase):
-    def paths(self):
-        I_DIR_TESTPDF = Path('tests/testdata')
-        for i_path in I_DIR_TESTPDF.glob('*.pdf'):
-            add_records_from_local_pdfpath(
-                database, config['propnames'], i_path)
-        add_records_from_local_pdfpath(
-            database, config['propnames'], I_DIR_TESTPDF)
+class TestPapntCLI(unittest.TestCase):
+    def setUp(self):
+        self.runner = CliRunner()
+        self.ot_dir_test = Path(mkdtemp())
+        self.in_path_config_test = self.ot_dir_test / 'config.toml'
+        IN_PATH_CONFIG = Path('PRIVATE/config_for_test.toml')
+        with open(IN_PATH_CONFIG, 'rb') as f:
+            config = tomllib.load(f)
+        self.tokenkey = config['database']['tokenkey']
+        self.database_id = config['database']['database_id']
 
-    def doi(self):
-        for doi in open('tests/testdata/doi-list-to-test', 'r').readlines():
-            database.create({'DOI':
-                {'rich_text': [{'text': {'content': doi.rstrip('\n')}}]}})
-        update_unchecked_records_from_doi(database, config['propnames'])
+    def tearDown(self):
+        shutil.rmtree(self.ot_dir_test)
 
-    def pdf(self):
-        update_unchecked_records_from_uploadedpdf(
-            database, config['propnames'])
+    def test_main(self):
+        with patch('papnt.misc.IN_PATH_CONFIG', self.in_path_config_test):
+            result = self.runner.invoke(main, catch_exceptions=False)
+            self.assertEqual(result.exit_code, 0)
 
-    def makebib(self):
-        target = 'test'
-        make_bibfile_from_records(
-            database, target, config['propnames'],
-            config['misc']['dir_save_bib'])
-        make_abbrjson_from_bibpath(
-            f'{config["misc"]["dir_save_bib"]}/{target}.bib',
-            config['abbr'])
+    def test_paths(self):
+        with patch('papnt.cli.IN_PATH_CONFIG', self.in_path_config_test), \
+             patch('papnt.misc.IN_PATH_CONFIG', self.in_path_config_test):
 
+            IN_DIR_TESTPDF = Path('tests/testdata')
+            in_paths = [str(in_path) for in_path in IN_DIR_TESTPDF.glob('*.pdf')]
+            result = self.runner.invoke(
+                main, ['paths'] + in_paths,
+                input=f'{self.tokenkey}\n{self.database_id}',
+                catch_exceptions=False)
+            if result.exit_code != 0:
+                print(result.exception)
+            self.assertEqual(result.exit_code, 0)
+
+    def test_doi(self):
+        database = NotionDatabase(self.tokenkey, self.database_id)
+        with open('tests/testdata/doi-list-to-test', 'r') as f:
+            for doi in f.readlines():
+                database.create({'DOI':
+                    {'rich_text': [{'text': {'content': doi.rstrip('\n')}}]}})
+        with patch('papnt.cli.IN_PATH_CONFIG', self.in_path_config_test), \
+             patch('papnt.misc.IN_PATH_CONFIG', self.in_path_config_test):
+
+            result = self.runner.invoke(
+                main, ['doi'], catch_exceptions=False,
+                input=f'{self.tokenkey}\n{self.database_id}')
+            if result.exit_code != 0:
+                print(result.exception)
+            self.assertEqual(result.exit_code, 0)
+
+    def test_pdf(self):
+        print('Manually upload test PDFs to Notion before running this test')
+        with patch('papnt.cli.IN_PATH_CONFIG', self.in_path_config_test), \
+             patch('papnt.misc.IN_PATH_CONFIG', self.in_path_config_test):
+
+            result = self.runner.invoke(
+                main, ['pdf'],
+                input=f'{self.tokenkey}\n{self.database_id}',
+                catch_exceptions=False)
+            if result.exit_code != 0:
+                print(result.exception)
+            self.assertEqual(result.exit_code, 0)
+
+    def test_makebib(self):
+        database = NotionDatabase(self.tokenkey, self.database_id)
+        with open('tests/testdata/doi-list-to-test', 'r') as f:
+            for doi in f.readlines():
+                database.create({
+                    'DOI': to_notionprop(doi.rstrip('\n'), 'rich_text'),
+                    'Cite in': to_notionprop(['test'], 'multi_select')})
+        with patch('papnt.cli.IN_PATH_CONFIG', self.in_path_config_test), \
+             patch('papnt.misc.IN_PATH_CONFIG', self.in_path_config_test):
+
+            # paths command
+            IN_DIR_TESTPDF = Path('tests/testdata')
+            in_paths = [str(in_path)
+                        for in_path in IN_DIR_TESTPDF.glob('*.pdf')]
+            result = self.runner.invoke(
+                main, ['paths'] + in_paths[0:2],
+                input=f'{self.tokenkey}\n{self.database_id}',
+                catch_exceptions=False)
+            if result.exit_code != 0:
+                print(result.exception)
+            self.assertEqual(result.exit_code, 0)
+
+            # doi command
+            result = self.runner.invoke(
+                main, ['doi'], catch_exceptions=False)
+            if result.exit_code != 0:
+                print(result.exception)
+            self.assertEqual(result.exit_code, 0)
+
+            # makebib command
+            result = self.runner.invoke(
+                main, ['makebib', 'test'],
+                input=str(Path.home() / 'Desktop'),
+                catch_exceptions=False)
+            if result.exit_code != 0:
+                print(result.exception)
+            self.assertEqual(result.exit_code, 0)
+
+    def test_fail_paths(self):
+        with patch('papnt.cli.IN_PATH_CONFIG', self.in_path_config_test), \
+             patch('papnt.misc.IN_PATH_CONFIG', self.in_path_config_test):
+
+            IN_DIR_TESTPDF = Path('tests/testdata/fail-to-record')
+            in_paths = [str(in_path)
+                        for in_path in IN_DIR_TESTPDF.glob('*.pdf')]
+            result = self.runner.invoke(
+                main, ['paths'] + in_paths,
+                input=f'{self.tokenkey}\n{self.database_id}')
+            if result.exit_code != 0:
+                print(result.exception)
+            self.assertEqual(result.exit_code, 0)
 
 if __name__ == '__main__':
-    Test().paths()
-    Test().doi()
-    Test().pdf()
-    input('Set "test" in "Cite in" of any record manually, and press key to continue...')
-    Test().makebib()
+    unittest.main()
